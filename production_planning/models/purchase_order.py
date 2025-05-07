65850476e71d2a93ac0b39a3c1e35121c4251058
1Ishani9 <ishani.manvar@setuconsulting.com>
2025-05-07 18:19:41 +0530

    [IM][MOD]Changes: - Fixed the issue of subcontracting not being created.

--- production_planning/models/purchase_order.py
+++ production_planning/models/purchase_order.py
@@ -23,3 +23,16 @@ class PurchaseOrder(models.Model):
             'planning_lines_id': planning_line_id and planning_line_id.id
         }
         return self.create(vals)
+
+    def button_confirm(self):
+        res = super(PurchaseOrder, self).button_confirm()
+        for purchase in self:
+            if purchase.subcontracting_resupply_picking_count:
+                resupply_picks = purchase._get_subcontracting_resupplies()
+                for pick in resupply_picks.filtered(lambda pick: pick.state not in ['done', 'cancel']):
+                    product_ids = pick.move_ids.mapped("product_id")
+                    pick.location_id = product_ids[0].source_location_id.id
+                for pick in self.picking_ids.filtered(lambda pick: pick.state not in ['done', 'cancel']):
+                    product_ids = pick.move_ids.mapped("product_id")
+                    pick.location_dest_id = product_ids[0].destination_location_id.id
+        return res
