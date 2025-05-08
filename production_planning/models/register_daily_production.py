d2d1ec94e35efd4c28f320d063cd159794c5beaa
ravik-setu <ravi@setuconsulting.com>
2025-05-08 13:06:10 +0530

    [RK][FIX]code and added store true in compute fields

--- production_planning/models/register_daily_production.py
+++ production_planning/models/register_daily_production.py
@@ -22,11 +22,9 @@ class RegisterDailyProduction(models.Model):
 
     def register_daily_production(self):
         action = {}
+        pending_data = {}
         mo = self.production_id
-        if not mo:
-            raise UserError("No any production order found!")
-        if mo.product_qty < self.qty_produced:
-            raise UserError("you are trying to produce more quantity")
+        self.raise_error_if_input_is_not_valid()
         mo.raise_error_if_multiple_lot_found()
         if mo.product_id.tracking != 'none' and not mo.lot_producing_id:
             mo.action_generate_serial()
@@ -36,10 +34,6 @@ class RegisterDailyProduction(models.Model):
         scrp_qty = self.scrap_qty
         to_consume = sum(mo.move_raw_ids.mapped('should_consume_qty')) if self.qty_produced else 0
         total_reserved = mo.get_available_component_qty_for_return()
-        if self.qty_produced and to_consume > total_reserved:
-            raise UserError(
-                "You are trying to use more quantity then reserved\nReserved: {},\nConsume: {}".format(total_reserved,
-                                                                                                       to_consume))
         _logger.info(
             "========= Start Register Production For MO {}:{} Quantity: {}, Reserved: {}".format(mo.id, mo.name,
                                                                                                  self.qty_produced,
@@ -55,7 +49,6 @@ class RegisterDailyProduction(models.Model):
             need_reserve_in_backorder -= scrp_qty
             mo.product_move_to_scrap(scrp_qty, mo.move_raw_ids.lot_ids)
         if self.qty_produced > 0:
-            pending_data = {}
             for move in mo.move_raw_ids:
                 if need_reserve_in_backorder > 0:
                     product_id = move.product_id
@@ -67,13 +60,15 @@ class RegisterDailyProduction(models.Model):
                 mo.move_raw_ids.write({'picked': True})
             action = mo.with_context(avoid_warning=True).mark_done_and_create_backorder_if_needed()
             self.planning_line.running_production_id = self.planning_line.find_latest_backorder() or mo.id
+            if self.planning_line.running_production_id and mo.id != self.planning_line.running_production_id.id:
+                self.planning_line.button_stop()
         if self.return_to_stock and self.return_quantity:
             try:
                 action = mo.return_product_to_stock(self.return_quantity)
             except Exception as e:
                 raise UserError(e)
-        self.reserve_backorder_based_on_pending_qty(
-            pending_data) if self.planning_line.running_production_id and mo.id != self.planning_line.running_production_id.id else self.planning_line.button_stop()
+        if self.planning_line.running_production_id and mo.id != self.planning_line.running_production_id.id:
+            self.reserve_backorder_based_on_pending_qty(pending_data)
         return action
 
     def reserve_backorder_based_on_pending_qty(self, pending_data):
@@ -86,3 +81,17 @@ class RegisterDailyProduction(models.Model):
                 self.env["stock.picking"].do_unreserve_and_reserve_based_on_lot(prod_moves, lot_ids,
                                                                                 qty=pending_data[product.id][
                                                                                     'quantity'])
+
+    def raise_error_if_input_is_not_valid(self):
+        total_reserved = self.production_id.get_available_component_qty_for_return()
+        to_consume = sum(self.production_id.move_raw_ids.mapped('should_consume_qty')) if self.qty_produced else 0
+        total_need = to_consume + self.scrap_qty + self.return_quantity
+        if not self.production_id:
+            raise UserError("No any production order found!")
+        if self.production_id.product_qty < self.qty_produced:
+            raise UserError("you are trying to produce more quantity")
+        if total_need > total_reserved:
+            raise UserError(
+                "You are trying to use more quantity then reserved\nReserved: {},\nProduction: {}\nReturn: {},\nScrap: {}".format(
+                    total_reserved,
+                    to_consume, self.return_quantity, self.scrap_qty))
